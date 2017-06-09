b33e47b3b9f95de1c8a7121d5d481cdec9b34eb7
2SC1815J <2SC1815J@users.noreply.github.com>
2017-06-09 21:25:17 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# ndldl_utils
+
+[国立国会図書館デジタルコレクション]向けユーザスクリプト
+
+
+国立国会図書館デジタルコレクションで資料を閲覧する際、次のカスタマイズを行います。
+- コマ移動に応じて、ブラウザのアドレスバーを現在表示中のコマのURLに更新
+- タイトルを「サイト名 - 資料名」から「資料名 - サイト名」に変更
+- 2013年9月27日のレイアウト変更で使いにくくなったデザインを応急的に変更
+
+[国立国会図書館デジタルコレクション]: http://dl.ndl.go.jp/
+[Firefox]: https://www.mozilla.org/firefox/
+[Greasemonkey]: https://addons.mozilla.org/firefox/addon/greasemonkey/
+
+## インストール方法
+
+1. このユーザスクリプトの利用には、[Greasemonkey]拡張機能がインストールされた[Firefox]が必要です。
+2. Greasemonkeyを初めてインストールした場合は、Firefoxを再起動してください。
+3. 以下のリンクをクリックすると、このユーザスクリプトのインストール確認が表示されます。
+    - [ndldl_utils.user.js](https://github.com/2SC1815J/ndldl_utils/raw/master/ndldl_utils.user.js)
+
+## 注意点
+
+国立国会図書館デジタルコレクションのサイトデザインが変更された場合、正しく動作しなくなる可能性があります。
---
+++ ndldl_utils.user.js
@@ -0,0 +1,85 @@
+// ==UserScript==
+// @name        ndldl_utils
+// @description 国立国会図書館デジタルコレクション閲覧画面のカスタマイズ
+// @author      2SC1815J
+// @date        2017-06-09
+// @license     MIT License
+// @namespace   https://github.com/2SC1815J
+// @homepageURL https://github.com/2SC1815J/ndldl_utils
+// @include     http://dl.ndl.go.jp/info:ndljp/pid/*
+// @require     https://gist.githubusercontent.com/BrockA/2625891/raw/9c97aa67ff9c5d56be34a55ad6c18a314e5eb548/waitForKeyElements.js
+// @version     0.2
+// ==/UserScript==
+
+// 従来
+// https://github.com/2SC1815J/kindigi_utils
+// として公開していたものの後継版
+
+(function ($) {
+  // 2013年9月27日のレイアウト変更で使いにくくなった箇所を応急的に変更する。
+  // 細かい辻褄合わせはしていない。対象の選択は手を抜いている。
+
+  // 以下のデザインは後日廃止されたのでコメントアウト
+  // 検索窓 → 赤色の枠は目に付きすぎるので変更。
+  // $('#search-textbox').css('border-color', 'transparent');
+  // 検索ボタン → 青色の枠は目に付きすぎるので変更。
+  // $('.mainbutton').css({'height': '23px', 'border-top-width': '0px', 'border-color': 'transparent', 'background-image': 'url("/resources/images/button-bg.png")'});
+
+  // コンテンツ表示エリアの各種ボタンが大きくなりすぎて使いにくいので小さくする。
+  // 概観図オン・表示領域設定・JPEG表示等の列のボタン下端が、画像の上端とちょうど
+  // 接してしまい、両者がひとつながりに見えて混乱するので、ボタンを小さくする。
+  // （ボタンアイコンが大きいままなのは仕様。余力のある方は直してください。）
+  waitForKeyElements(
+    '.minibutton, .mb-placeholder',
+    function(jNode) {
+      jNode.css({'height': '20px', 'font-weight': 'normal', 'font-size': '100%'});
+      $('#btn-help').css({'right': '25px', 'top': '7px'}); //ついでに修正
+    }
+  );
+  waitForKeyElements(
+    '.imagecontrol',
+    function(jNode) {
+      jNode.css('padding-bottom', '6px');
+    }
+  );
+
+  // その後行われたデザイン変更に対するCSSカスタマイズ（目立ちすぎる配色を抑えるなど）
+  $('.detailcontrolbox').css({'line-height': '30px', 'height': '30px'});
+  $('.searchotherdata').find('br').remove();
+  $('.searchotherdata').css({'width': '270px !important', 'padding-right': '0px'});
+  $('.backtoresult, .searchotherdata').css('font-size', '12px');
+  $('.backtoresult a, .searchotherdata a').css('font-weight', 'normal');
+  $('.searchotherdata a span').css('background-color', 'transparent');
+  $('.toctabopened a').css('background-position', '0 -25px');
+  $('#GlobalNaviArea').css({'padding-top': '10px', 'padding-bottom': '10px'});
+
+  // タブやブックマークで識別しやすくするため、タイトルを「資料名 - サイト名」に変更する。
+  function fixTitle() {
+    var match = document.title.match(/(国立国会図書館デジタルコレクション) - (.+)/i);
+    if (match) {
+      document.title = match[2] + ' - ' + match[1];
+    }
+  }
+
+  var target = document.querySelector('head > title');
+  var observer = new MutationObserver(function(MutationRecords, MutationObserver) {
+    fixTitle();
+    // ブラウザのアドレスバーを、現在表示中のコマのURLに更新する。
+    var val = $('#sel-content-no').val();
+    if (history.replaceState && history.state !== undefined && val) {
+        var url = location.protocol + '//' + location.host + location.pathname;
+        var match = url.match(/http:\/\/dl\.ndl\.go\.jp\/info:ndljp\/pid\/([0-9]+)(\/[0-9]+)?/i);
+        if (match) {
+          var newUrl = 'http://dl.ndl.go.jp/info:ndljp/pid/' + match[1];
+          if (val > 1) {
+            newUrl += '/' + val;
+          }
+          history.replaceState(null, null, newUrl);
+        }
+    }
+  });
+  observer.observe(target, {subtree: true, characterData: true, childList: true});
+
+  fixTitle();
+
+})(jQuery);
